d78905f7f2f4300f5103e987fd6a7efab8ceae3b
brossaip <brossaip@gmail.com>
2017-11-06 17:57:34 +0100

    Adding discharging symbol in brainy theme
    I have followed the atomic theme

--- themes/brainy/brainy.theme.bash
+++ themes/brainy/brainy.theme.bash
@@ -163,8 +163,10 @@ ___brainy_prompt_battery() {
 	elif [ "$info" -lt 25 ]; then
 		color=$bold_red
 	fi
-	box="[|]"
-	ac_adapter_connected && info+="+"
+	box="[|]"	
+  ac_adapter_connected && charging="+"
+  ac_adapter_disconnected && charging="-" 
+  info+=$charging
 	[ "$info" == "100+" ] && info="AC"
 	printf "%s|%s|%s|%s" "${color}" "${info}" "${bold_white}" "${box}"
 }
